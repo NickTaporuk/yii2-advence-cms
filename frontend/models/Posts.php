337c15148e0fa14db4c93577bed0e8cc512a9e16
NickTaporuk <nictaporuk@yandex.ru>
2015-03-21 08:11:12 +0200

    add

---
+++ frontend/models/Posts.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "posts".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $text
+ * @property string $text_preview
+ * @property string $img
+ */
+class Posts extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'posts';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['text'], 'string'],
+            [['title', 'text_preview', 'img'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'title' => Yii::t('app', 'Title'),
+            'text' => Yii::t('app', 'Text'),
+            'text_preview' => Yii::t('app', 'Text Preview'),
+            'img' => Yii::t('app', 'Img'),
+        ];
+    }
+}
