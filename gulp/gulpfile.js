98b75208f3f78a8b50e8681d3ee3d9327f3ef999
NickTaporuk <nictaporuk@yandex.ru>
2015-03-31 00:07:27 +0300

    add gulp plugin

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
+var concatCss = require('gulp-concat-css'),
+    minifyCSS = require('gulp-minify-css'),
+    renameCss = require('gulp-rename');
 
 gulp.task('default', function () {
     //путь к папке
-    return gulp.src('../frontend/web/css/*.css')
-        .pipe(concatCss("../frontend/web/css/bundle.css"))
-        .pipe(gulp.dest('out/'));
+    return gulp.src('/var/www/advanced/frontend/web/css/*.css')
+        .pipe(concatCss("bundle.css"))
+        .pipe(minifyCSS())
+        .pipe(renameCss("bundle.min.css"))
+        .pipe(gulp.dest('../frontend/web/css/minify/'));
+});
+
+gulp.task('watch',function(){
+    gulp.watch('/var/www/advanced/frontend/web/css/*.css',['default']);
 });
